6ebdb95755c36c2297c788517a7b26648e1bfaf4
atetheone <atevirran@gmail.com>
2023-01-15 21:17:54 +0100

    app.js updated

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express     = require('express');
+const cors        = require('cors');
+
+const { indexRouter } = require('./routes/index');
+const { apiRouter } = require('./routes/api');
+
+let app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors()); //For FCC testing purposes only
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(indexRouter);
+
+//Routing for API 
+app.use(apiRouter);
+    
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+
+module.exports = app; //for testing
