aa8f1714e180ef2ee8bcab19a8a88858d8b12458
atetheone <atevirran@gmail.com>
2023-01-15 21:43:35 +0100

    require testing files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express     = require('express');
 const cors        = require('cors');
-
+const fccTestingRoutes  = require('./routes/fcctesting.js');
 const { indexRouter } = require('./routes/index');
 const { apiRouter } = require('./routes/api');
 
@@ -12,13 +12,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors()); //For FCC testing purposes only
 
-
+fccTestingRoutes(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 app.use(indexRouter);
+
 
 //Routing for API 
 app.use(apiRouter);
--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const runner = require('./test-runner');
 require("dotenv").config();
 
 mongoose.set('strictQuery', true);
@@ -9,17 +10,17 @@ const startDB = async (app, port) => {
     console.log("Connected successfully to the DB");
     app.listen(port, () => {
       console.log('Your app is listening on port ' + port);
-      // if (process.env.NODE_ENV === 'test') {
-      //   console.log('Running Tests...');
-      //   setTimeout(function () {
-      //     try {
-      //       runner.run();
-      //     } catch(e) {
-      //       console.log('Tests are not valid:');
-      //       console.error(e);
-      //     }
-      //   }, 15000);
-      // }
+      if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function () {
+          try {
+            runner.run();
+          } catch(e) {
+            console.log('Tests are not valid:');
+            console.error(e);
+          }
+        }, 15000);
+      }
     });
   } catch (err) {
     console.log("Unable to connect to the DB");
