da5dd4cf0c53befe1a65245eaa0546590f5b0027
atetheone <atevirran@gmail.com>
2023-01-15 21:06:52 +0100

    database setup

---
+++ db.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.set('strictQuery', true);
+const startDB = async (app, port) => {
+  const mongo_uri = process.env.MONGO_URI || "";
+  try {
+    await mongoose.connect(mongo_uri);
+    console.log("Connected successfully to the DB");
+    app.listen(port, () => {
+      console.log('Your app is listening on port ' + port);
+      // if (process.env.NODE_ENV === 'test') {
+      //   console.log('Running Tests...');
+      //   setTimeout(function () {
+      //     try {
+      //       runner.run();
+      //     } catch(e) {
+      //       console.log('Tests are not valid:');
+      //       console.error(e);
+      //     }
+      //   }, 15000);
+      // }
+    });
+  } catch (err) {
+    console.log("Unable to connect to the DB");
+  }
+};
+
+module.exports = startDB;
